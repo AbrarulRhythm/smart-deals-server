c8e94a81778a6c579007b4892dda57668156cc37
Abrarul Rhythm <[....email>
2025-10-31 01:06:18 +0600

    setup mongobd and creat add product api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +8,42 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://smartdbUser:20gqB3EHJhR0JmjJ@cluster0.qrthjko.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Smart server is running');
 })
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('smart_db');
+        const productsCollection = db.collection('products');
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Smart server is running on port: ${port}`);
